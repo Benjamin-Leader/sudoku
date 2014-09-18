23d7fd9d0e442f7f8ae753026b3435bc91c6a9ae
=Ben Leader <=>
2014-09-18 14:15:52 -0700

    Initial branch commit gridModel class

---
+++ sudoku/BLXWGridModel.h
@@ -0,0 +1,13 @@
+//
+//  BLXWGridModel.h
+//  sudoku
+//
+//  Created by Ben Leader on 9/18/14.
+//  Copyright (c) 2014 Benjamin Leader and Hannah Long. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BLXWGridModel : NSObject
+
+@end
---
+++ sudoku/BLXWGridModel.m
@@ -0,0 +1,13 @@
+//
+//  BLXWGridModel.m
+//  sudoku
+//
+//  Created by Ben Leader on 9/18/14.
+//  Copyright (c) 2014 Benjamin Leader and Hannah Long. All rights reserved.
+//
+
+#import "BLXWGridModel.h"
+
+@implementation BLXWGridModel
+
+@end
